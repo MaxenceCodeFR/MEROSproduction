2f0b5fb54371f9b43b79ebc2eb970432ae264f0c
MaxenceCodeFR <maxence.hattabi@gmail.com>
2024-01-05 11:26:52 +0100

    ajout des réseaux sociaux, cetait le bordel mais on est là

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -20,6 +20,34 @@ class TeamController extends AbstractController
         $teams = $userRepository->findUsersWithRoleCEO();
         dump($teams);
 
+        //Pour eviter de faire une boucle dans le twig
+        //je vais créer un tableau d'objet avec les réseaux sociaux
+        // Je boucle sur les utilisateurs stocker dans la varibale $teams
+        foreach ($teams as $team) {
+            //Je récupère leurs réseaux sociaux sachant que c'est une Collection
+            $socialsCollection = $team->getSocial();
+            //Initialisation d'un tableau vide pour stocker les informations de réseaux sociaux pour chaque membre de l'équipe.
+            $team->socialNetworks = [];
+
+            //Comme c'est une collection d'une relation ManyToMany
+            //Je boucle sur la collection pour récupérer les réseaux sociaux
+            //A faire quand l'erreur est : "PersistentCollection" to string conversion
+            //Attention c'est un tableau d'objet indexé
+            foreach ($socialsCollection as $social) {
+                $team->socialNetworks[] = [
+                    'Instagram' => $social->getInstagram(),
+                    'Snapchat' => $social->getSnapchat(),
+                    'TikTok' => $social->getTiktok(),
+                    'Twitter' => $social->getTwitter(),
+                    'Facebook' => $social->getFacebook(),
+                    'Pinterest' => $social->getPinterest(),
+                    'Website' => $social->getWebsite(),
+                    'Youtube' => $social->getYoutube(),
+
+                ];
+            }
+        }
+
         return $this->render('team/index.html.twig', compact('teams'));
     }
 }
