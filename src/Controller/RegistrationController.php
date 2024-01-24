981b6b128897d971ac5e2f457c36759de0c72f90
MaxenceCodeFR <maxence.hattabi@gmail.com>
2024-01-24 09:43:36 +0100

    envoi de mail d inscription ok

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,25 +4,46 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
-    {
+    public function register(
+        Request $request,
+        UserPasswordHasherInterface $userPasswordHasher,
+        EntityManagerInterface $entityManager,
+        UserRepository $user,
+        MailerInterface $mail
+    ): Response {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $user->setRoles(["ROLE_USER"]);
+            //Obliger de mettre un token vide car ne peut pas être 'null'
+            $user->setResetToken('');
+
+            $email = (new TemplatedEmail())
+                ->from('no-reply@meros-production')
+                ->to($user->getEmail())
+                ->subject('Bienvenue sur Meros Production')
+                ->htmlTemplate('emails/welcome.html.twig')
+                ->context([
+                    'user' => $user,
+                ]);
+
+            $mail->send($email);
+
             // encode the plain password
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
@@ -33,9 +54,8 @@ class RegistrationController extends AbstractController
 
             $entityManager->persist($user);
             $entityManager->flush();
-            // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('landing');
+            return $this->redirectToRoute('login');
         }
 
         return $this->render('registration/register.html.twig', [
