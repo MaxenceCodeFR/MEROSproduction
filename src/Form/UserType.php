feacfe595104cf19c0a9aca9b5e6544798903093
MaxenceCodeFR <maxence.hattabi@gmail.com>
2024-04-25 16:52:02 +0200

    sécu max des formulaires

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,45 +6,86 @@ use App\Entity\User;
 use App\Entity\Specialty;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\Image;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            // ->add('roles', null, [
-            //     'attr' => ['class' => 'json-array-field'] // Indiquer que 'roles' est un champ JSON
-            // ])
-            ->add('firstname')
-            ->add('lastname')
-            ->add('text')
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'L\'adresse email est requise.'
+                    ]),
+                    new Email([
+                        'message' => 'Veuillez fournir une adresse email valide.'
+                    ])
+                ]
+            ])
+            ->add('firstname', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le prénom est requis.'
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 100,
+                        'minMessage' => 'Le prénom doit avoir au moins {{ limit }} caractères.',
+                        'maxMessage' => 'Le prénom ne peut pas excéder {{ limit }} caractères.'
+                    ])
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Le nom est requis.'
+                    ]),
+                    new Length([
+                        'min' => 2,
+                        'max' => 100,
+                        'minMessage' => 'Le nom doit avoir au moins {{ limit }} caractères.',
+                        'maxMessage' => 'Le nom ne peut pas excéder {{ limit }} caractères.'
+                    ])
+                ]
+            ])
+            ->add('text', TextType::class, [
+                'required' => false,
+                'constraints' => [
+                    new Length([
+                        'max' => 1000,
+                        'maxMessage' => 'Le texte ne peut pas excéder {{ limit }} caractères.'
+                    ])
+                ]
+            ])
             ->add('social', CollectionType::class, [
                 'entry_type' => SocialType::class,
-                'entry_options' => [
-                    'label' => false, // pour ne pas afficher le label "Socials"
-                ],
+                'entry_options' => ['label' => false],
                 'allow_add' => true,
                 'allow_delete' => true,
                 'by_reference' => false,
             ])
             ->add('image', FileType::class, [
-                'label' => 'Image (Fichier WebP)',
-                'mapped' => false, 
+                'label' => 'Image (Fichier WebP, JPEG, PNG)',
+                'mapped' => false,
                 'required' => false,
                 'constraints' => [
                     new Image([
                         'maxWidth' => 500,
                         'maxHeight' => 500,
-                        'mimeTypes' => ['image/webp'],
-                        'mimeTypesMessage' => 'Veuillez télécharger une image valide au format WebP.',
+                        'mimeTypes' => ['image/webp', 'image/jpeg', 'image/png'],
+                        'mimeTypesMessage' => 'Veuillez télécharger une image valide (WebP, JPEG, PNG).',
                     ]),
                     new File([
                         'maxSize' => '1024k',
@@ -54,10 +95,11 @@ class UserType extends AbstractType
             ])
             ->add('specialty', EntityType::class, [
                 'class' => Specialty::class,
-                'choice_label' => 'name', // Utiliser le champ 'name' de Specialty
+                'choice_label' => 'name',
                 'multiple' => true,
                 'expanded' => true,
             ]);
+
         if ($options['isCEO']){
             $builder->add('isFamous', CheckboxType::class, [
                 'label' => 'Cocher si l\'influenceur est célèbre',
@@ -70,6 +112,7 @@ class UserType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'isCEO' => false,
         ]);
         $resolver->setRequired('isCEO');
     }
